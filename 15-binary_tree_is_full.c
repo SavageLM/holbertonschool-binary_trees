42d26069c599bf76f5852d7627ed0bf4f83e4f38
SavageLM <nagol.egavas@gmail.com>
2023-08-21 19:33:34 -0700

    Added 15-binary_tree_is_full file and code

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if tree is full
+ * @tree: tree to check
+ * Return: returns 0 if tree is null
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (full_recursion(tree));
+}
+
+/**
+ * full_recursion - recursive function to check if tree is full
+ * @tree: tree to check
+ * Return: returns 1 if full, 0 if not
+ */
+
+int full_recursion(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((tree->left != NULL && tree->right == NULL) || 
+				(tree->left == NULL && tree->right != NULL) ||
+				binary_tree_is_full(tree->left) == 0 || 
+				binary_tree_is_full(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
