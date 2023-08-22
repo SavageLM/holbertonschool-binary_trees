ef12550a249e3f2f105277c9cf69960e384a7233
SavageLM <nagol.egavas@gmail.com>
2023-08-21 19:53:34 -0700

    Fixing betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -23,9 +23,9 @@ int full_recursion(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if ((tree->left != NULL && tree->right == NULL) || 
+		if ((tree->left != NULL && tree->right == NULL) ||
 				(tree->left == NULL && tree->right != NULL) ||
-				binary_tree_is_full(tree->left) == 0 || 
+				binary_tree_is_full(tree->left) == 0 ||
 				binary_tree_is_full(tree->right) == 0)
 			return (0);
 	}
