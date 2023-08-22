86eef57ee9a08f4053e218ce5daf712d619782f4
SavageLM <nagol.egavas@gmail.com>
2023-08-21 21:09:55 -0700

    reswapped branches in 14-balance, set find_height to add 1 more

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->right) - binary_tree_height(tree->left));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
@@ -27,7 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 	return ((left > right) ? left : right);
 }
